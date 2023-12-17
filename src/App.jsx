b1861ace24cad8c14f29009356fb092dbafea031
erkhes01 <106813277+erkheseee@users.noreply.github.com>
2023-12-17 13:55:42 +0800

    fetched data from pokemon api

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [images, setImages] = useState([]);
+
+  function fetchPokemonData(pokemon) {
+    let url = pokemon.url // <--- this is saving the pokemon url to a      variable to us in a fetch.(Ex: https://pokeapi.co/api/v2/pokemon/1/)
+    fetch(url)
+      .then(response => response.json())
+      .then(function (pokeData) {
+        let object = {};
+        object.src = pokeData['sprites']['front_default'];
+        object.key = Math.random();
+        setImages(current => [...current, object]);
+      })
+  }
+
+  const retrieveData = async () => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=6&offset=30')
+      .then(response => response.json())
+      .then(function (allpokemon) {
+        allpokemon.results.forEach(function (pokemon) {
+          fetchPokemonData(pokemon);
+        })
+      })
+  }
+
+  useEffect(() => {
+    retrieveData();
+  }, []);
+
+  console.log(images);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='App'>
+        {images.map(image => (
+          <div key={image.key}><img src={image.src} alt={image.src} ></img></div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
